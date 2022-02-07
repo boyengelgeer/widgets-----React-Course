310ee074cb086c5e7271b9f44355868570855a1f
Boy Engelgeer <boyengelgeer@gmail.com>
2022-02-07 14:32:22 +0100

    Second widget
    Second widget is done, wikipedia search which uses useState, useEffect and explains the timeout function. Working on third widget started

---
+++ src/components/Dropdown.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Dropdown = ({ options }) => {
+  const renderedOptions = options.map((option) => {
+    return (
+      <div key={option.value} className="item">
+        {option.label}
+      </div>
+    );
+  });
+  return <h1>Dropdown</h1>;
+};
+
+export default Dropdown;
