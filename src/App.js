3136ba2d15f91be5495f6cbd18ec75aef7394b32
Boy Engelgeer <boyengelgeer@gmail.com>
2022-02-08 08:52:11 +0100

    4th Widget done
    4th Widget done, translation widget with google API.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Accordion from "./components/Accordion";
 import Search from "./components/Search";
 import Dropdown from "./components/Dropdown";
+import Translate from "./components/Translate";
 
 const items = [
   {
@@ -34,20 +35,9 @@ const options = [
 ];
 
 const App = () => {
-  const [selected, setSelected] = useState(options[0]);
-  const [showDropdown, setShowdropdown] = useState(true);
   return (
     <div>
-      <button onClick={() => setShowdropdown(!showDropdown)}>
-        Toggle Dropdown
-      </button>
-      {showDropdown ? (
-        <Dropdown
-          selected={selected}
-          onSelectedChange={setSelected}
-          options={options}
-        />
-      ) : null}
+      <Translate />
     </div>
   );
 };
